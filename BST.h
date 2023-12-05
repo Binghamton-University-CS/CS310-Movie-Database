0511f17792d770cb933ff5caee76515f0150def1
sambuckler1 <120116786+sambuckler1@users.noreply.github.com>
2023-12-05 15:12:25 -0500

    BSTUpdate

--- BST.h
+++ BST.h
@@ -21,6 +21,7 @@ class BST{
         bool remove(string key);
         Node* getRoot();
         Node* find(string key);
+        Node* BST::findMin(Node* node);
 
         //extra, probably wont need:
         void inOrderTraversal();
@@ -29,99 +30,95 @@ class BST{
 };
 
 void BST::insert(string key, int index){
-    
-    currNode* = root;
+    if(root == nullptr){
+        root = new Node(key);
+        root -> arrayIndex = arrayIndex;
+        return;
+    }
+    Node* currNode = root;
     while(1){
-        if(key > currNode.key){
+        if(key > currNode->key){
             if(currNode -> right == nullptr){
-                currNode -> right = new Node(key, index);
+                currNode -> right = new Node(key);
+                currNode -> right -> arrayIndex = index;
                 break;
             }
             else currNode = currNode -> right;
         }
-        if(key < currNode.key){
+        if(key < currNode->key){
             if(currNode -> left == nullptr){
-                currNode -> left = new Node(key, index);
+                currNode -> left = new Node(key);
+                currNode ->left -> arrayIndex = index;
                 break;
             }
             else currNode = currNode -> left;
         }
     }
 }
-bool BST::remove(string key){  
-    currNode* = root;
-    while(currNode -> left.key != key && currNode -> right.key != key){
-        if(key > currNode.key){
-            if(currNode -> right == nullptr){
-                return false; //node DNE
-            }
-            currNode = currNode -> right;
-        }
-        if(key < currNode.key){
-            if(currNode -> left == nullptr){
-                return false; //node DNE
-            }
-            currNode = currNode -> left;
-        }
+bool BST::remove(Node*& node, string key) {
+    if (node == nullptr) {
+        return false; // Key not found
     }
-    if(currNode -> left.key == key){
-        //case 0: node to be removed is has no children
-        if(!currNode -> left && !currNode -> right){
-            currNode -> left == nullptr;
-        } 
-        //case 1: node to be removed has 1 child.
-        if(currNode -> left -> left != nullptr && currNode -> left -> right == nullptr){
-            //just left child exists
-            currNode -> left = currNode -> left -> left;
+
+    if (key < node->key) {
+        return remove(node->left, key); // Key is in the left subtree
+    } else if (key > node->key) {
+        return remove(node->right, key); // Key is in the right subtree
+    } else {
+        // Node with key found
+        if (node->left == nullptr && node->right == nullptr) {
+            // Node is a leaf
+            delete node;
+            node = nullptr;
+        } else if (node->left != nullptr && node->right != nullptr) {
+            // Node has two children
+            Node* successor = findMin(node->right);
+            node->key = successor->key;
+            node->arrayIndex = successor->arrayIndex; // If you're keeping arrayIndex in nodes
+            return remove(node->right, successor->key); // Remove the successor
+        } else {
+            // Node has one child
+            Node* child = (node->left != nullptr) ? node->left : node->right;
+            Node* oldNode = node;
+            node = child;
+            delete oldNode;
         }
-        else if(currNode -> left -> left == nullptr && currNode -> left -> right != nullptr){
-            //just right child exists
-            currNode -> left = currNode -> left -> right;
-        }
-        //case 2: left has 2 children
-        if(currNode -> left -> left != nullptr && currNode -> left -> right != nullptr){
-            Node* successor = currNode -> left;
-            while(successor -> right){
-                successor = successor -> right;
-            }
-            successor -> left = currNode -> left -> left;
-            successor -> right = currNode -> left -> right;
-            currNode -> left = successor;
-        }
-    }
-    if(currNode -> right.key == key){
-        //Copy everything from above into here, but change currNode-> left to right
+        return true;
     }
 }
+
+Node* BST::findMin(Node* node) {
+    if(node == nullptr){
+        return nullptr;
+    }
+    while (node->left != nullptr) {
+        node = node->left;
+    }
+    return node;
+}
+
+// Public interface to remove function
+bool BST::remove(string key) {
+    return remove(root, key);
+}
+
 
 Node* BST::getRoot(){
     return root;
 }
 
 Node* BST::find(string key){
-    currNode* = root;
+    Node* currNode = root;
     while(currNode != nullptr){
-        if(key > currNode.key){
+        if(key > currNode->key){
                 currNode = currNode -> right;
         }
-        else if(key < currNode.key){
+        else if(key < currNode->key){
             currNode = currNode -> left;
         }
-        else if(key == currNode.key){ //found
+        else if(key == currNode->key){ //found
             return currNode;
         }
     }
     return nullptr; //did not find
 }
-
-// Node* BST::insert(string key){
-//     currNode* = root;
-//     if()
-//     if(key > currNode.key){
-//         currNode = currNode -> right;
-//     }
-//     else if(key < currNode -> left){
-//         currNode = currNode -> left;
-//     }
-    
-// }
