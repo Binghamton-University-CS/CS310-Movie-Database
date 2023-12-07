a64f9fdcc9719852c9c47492ecf4354515389344
sambuckler1 <120116786+sambuckler1@users.noreply.github.com>
2023-12-07 17:25:21 +0000

    updaet

--- Heap.h
+++ Heap.h
@@ -7,13 +7,13 @@
 using namespace std;
 class Heap{
     private:
-        Array<int> *Actor.h; 
+        Array<int> Actor; 
         
     public:
         int Perlocatedown(int index);
         int Perlocateup(int index); //returns the index where its inserted 
         void Extractmax();
-        int Insert(Actor* actor); //returns the index where its inserted
+        int Insert(Actor& actor); //returns the index where its inserted
         void Delete(int index); //should be bool
         bool updateNode(int index, int num); //updates praise points, returns bool 
         //int getArrayIndex(praise_points);
